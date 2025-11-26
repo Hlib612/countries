7c9fb907fcb7b3a25932ddf7f847ac2a5da66180
Hlib612 <glebmalonos@gmail.com>
2025-11-26 19:48:23 +0200

    add some

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const searchInput = document.querySelector('.search_input');
+const countryList = document.querySelector('.country_list');
 const BASE_URL = 'https://restcountries.com/v3.1/';
 
 let searchInputLength = 0;
@@ -8,9 +9,30 @@ const onTyping = event => {
   let query = event.currentTarget.value;
   // console.log(event.currentTarget.value.length)
   // searchInputLength = event.currentTarget.value.length;
+
   fetchCountries(query).then(result => {
     if (result.length > 10) {
       alert('stop');
+    }else if(result.length === 1){
+
+    } else {
+      countryList.textContent = '';
+      result.map(country => {
+        // console.log(country)
+        // console.log(country.name)
+        console.log(country.name.common)
+        console.log(countryList.textContent);
+        console.log(countryList.textContent.includes(country.name.common));
+
+                if(countryList.textContent.includes(country.name.common)){
+                  console.log('Well well well')
+                  return ;
+                } else{
+        countryList.insertAdjacentHTML('beforeend', `<li>${country.name.common}</li>`)
+                }
+
+        console.log(countryList);
+      });
     }
     console.log(result);
   });
