bb784b285d3c274aaafcd5967dba3c0068928584
Hlib <glebmalonos@gmail.com>
2025-12-26 10:14:13 +0200

    add some

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
 const searchInput = document.querySelector('.search_input');
 const countryList = document.querySelector('.country_list');
 const BASE_URL = 'https://restcountries.com/v3.1/';
-const countryTamplate = document.querySelector("#countries-templ")
-const alertEl = document.querySelector(".js-alert");
+const countryTamplate = document.querySelector('#countries-templ');
+const alertEl = document.querySelector('.js-alert');
 
-const template = Handlebars.compile(countryTamplate.innerHTML)
+const template = Handlebars.compile(countryTamplate.innerHTML);
 
 const showNotification = () => {
-alertEl.classList.add("is-visible");
-}
+  alertEl.classList.add('is-visible');
+};
 
-
-const clearNotification = () =>{
-alertEl.classList.remove("is-visible");
-console.log("well well well")
-}
-
-
-
+const clearNotification = () => {
+  alertEl.classList.remove('is-visible');
+  console.log('well well well');
+};
 
 // const onNotificationClick = () => {
 //     clearNotification();
@@ -26,49 +22,51 @@ console.log("well well well")
 
 let searchInputLength = 0;
 
-const onTyping = event => {
-  console.log(event.currentTarget.value);
-  let query = event.currentTarget.value;
-  // console.log(event.currentTarget.value.length)
-  // searchInputLength = event.currentTarget.value.length;
-
-
-  const fetchCountries = name => {
+const fetchCountries = name => {
   return fetch(`${BASE_URL}name/${name}`).then(response => {
     // console.log(response)
     return response.json();
   });
 };
 
+const onTyping = event => {
+  console.log(event.currentTarget.value);
+  let query = event.currentTarget.value;
+  // console.log(event.currentTarget.value.length)
+  // searchInputLength = event.currentTarget.value.length;
+
   fetchCountries(query).then(result => {
     if (result.length > 10) {
-showNotification()
-let timerId = setTimeout(clearNotification , 3000);
-      countryList.textContent = '';
-    }else if(result.length === 1){
-countryList.textContent = '';
+      showNotification();
+      let timerId = setTimeout(clearNotification, 3000);
+      countryList.innerHTML = '';
+    } else if (result.length === 1) {
+      countryList.innerHTML = '';
       result.map(country => {
-        console.log(country)
-        console.log(country.flag.png)
-        const murkup = template(country)
-        
-countryList.innerHTML = murkup;
-      })
+        console.log(country);
+        console.log(country.flag.png);
+        const murkup = template(country);
+
+        countryList.innerHTML = murkup;
+      });
     } else {
       countryList.textContent = '';
       result.map(country => {
         // console.log(country)
         // console.log(country.name)
-        console.log(country.name.common)
+        console.log(country.name.common);
         console.log(countryList.textContent);
         console.log(countryList.textContent.includes(country.name.common));
 
-                if(countryList.textContent.includes(country.name.common)){
-                  console.log('Well well well')
-                  return ;
-                } else{
-        countryList.insertAdjacentHTML('beforeend', `<li>${country.name.common}</li>`)
-                }
+        if (countryList.textContent.includes(country.name.common)) {
+          console.log('Well well well');
+          return;
+        } else {
+          countryList.insertAdjacentHTML(
+            'beforeend',
+            `<li>${country.name.common}</li>`
+          );
+        }
 
         console.log(countryList);
       });
@@ -78,5 +76,3 @@ countryList.innerHTML = murkup;
 };
 
 searchInput.addEventListener('input', onTyping);
-
-
